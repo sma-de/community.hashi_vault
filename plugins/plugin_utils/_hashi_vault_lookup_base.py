383992269838a6c5e8160a0525eaf1c754fd92ae
Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
2023-01-02 16:23:39 +0100

    wup

--- plugins/plugin_utils/_hashi_vault_lookup_base.py
+++ plugins/plugin_utils/_hashi_vault_lookup_base.py
@@ -16,7 +16,7 @@ from ansible.errors import AnsibleError
 from ansible.plugins.lookup import LookupBase
 from ansible.utils.display import Display
 
-from ansible_collections.community.hashi_vault.plugins.plugin_utils._hashi_vault_plugin import HashiVaultPlugin
+from ansible_collections.smabot.hvtest.plugins.plugin_utils._hashi_vault_plugin import HashiVaultPlugin
 
 display = Display()
 
--- plugins/plugin_utils/_hashi_vault_plugin.py
+++ plugins/plugin_utils/_hashi_vault_plugin.py
@@ -16,13 +16,13 @@ from ansible.plugins import AnsiblePlugin
 from ansible import constants as C
 from ansible.utils.display import Display
 
-from ansible_collections.community.hashi_vault.plugins.module_utils._hashi_vault_common import (
+from ansible_collections.smabot.hvtest.plugins.module_utils._hashi_vault_common import (
     HashiVaultHelper,
     HashiVaultOptionAdapter,
 )
 
-from ansible_collections.community.hashi_vault.plugins.module_utils._connection_options import HashiVaultConnectionOptions
-from ansible_collections.community.hashi_vault.plugins.module_utils._authenticator import HashiVaultAuthenticator
+from ansible_collections.smabot.hvtest.plugins.module_utils._connection_options import HashiVaultConnectionOptions
+from ansible_collections.smabot.hvtest.plugins.module_utils._authenticator import HashiVaultAuthenticator
 
 
 display = Display()
