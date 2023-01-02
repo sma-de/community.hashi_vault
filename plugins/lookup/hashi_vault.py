1597a18409a221db8e55799e42eab8da657e69fd
Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
2023-01-02 16:15:42 +0100

    wup

--- plugins/lookup/hashi_vault.py
+++ plugins/lookup/hashi_vault.py
@@ -239,8 +239,8 @@ _raw:
 from ansible.errors import AnsibleError
 from ansible.utils.display import Display
 
-from ansible_collections.community.hashi_vault.plugins.plugin_utils._hashi_vault_lookup_base import HashiVaultLookupBase
-from ansible_collections.community.hashi_vault.plugins.module_utils._hashi_vault_common import HashiVaultValueError
+from ansible_collections.smabot.hvtest.plugins.plugin_utils._hashi_vault_lookup_base import HashiVaultLookupBase
+from ansible_collections.smabot.hvtest.plugins.module_utils._hashi_vault_common import HashiVaultValueError
 
 display = Display()
 
